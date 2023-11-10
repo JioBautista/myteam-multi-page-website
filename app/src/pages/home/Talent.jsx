1bdb60d08fff910a4d8b28d493b539b7bdc821b9
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2023-11-10 17:45:35 -0500

    test

--- app/src/pages/home/Talent.jsx
+++ app/src/pages/home/Talent.jsx
@@ -4,14 +4,14 @@ import styles from "../../styles/home.module.scss";
 function Talent() {
   return (
     <div className={styles.talent}>
-      <h1>
-        Find the best <span>talent</span>
-      </h1>
-      <p>
-        Finding the right people and building high performing teams can be hard.
-        Most companies aren't tapping into the abundance of global talent. We're
-        about to change that.
-      </p>
+        <h1>
+          Find the best <span>talent</span>
+        </h1>
+        <p>
+          Finding the right people and building high performing teams can be
+          hard. Most companies aren't tapping into the abundance of global
+          talent. We're about to change that.
+        </p>
     </div>
   );
 }
