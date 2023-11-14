ad5380c1a940c50ebd533d5a3440eee37be1db9d
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2023-11-14 15:34:45 -0500

    Active states started

--- app/src/navbar/NavBar.jsx
+++ app/src/navbar/NavBar.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import styles from "./navbar.module.scss";
-import Button from "../components/Button";
 import { Link } from "react-router-dom";
 
 function NavBar() {
@@ -12,15 +11,16 @@ function NavBar() {
           <Link to="/">
             <img src="src/assets/logo.svg" />
           </Link>
+
           <Link to="/" className={styles.navlink}>
             home
           </Link>
+
           <Link to="about" className={styles.navlink}>
             about
           </Link>
-          <button to="contact" className={styles.navbtn}>
-            <Link to="contact">Contact Us</Link>
-          </button>
+
+          <Link to="contact" className={styles.navbtn}>Contact Us</Link>
           <img
             src="src/assets/icon-hamburger.svg"
             onClick={() => setIsOpen(!false)}
@@ -34,10 +34,7 @@ function NavBar() {
               />
               <Link to="/">home</Link>
               <Link to="about">about</Link>
-              <Button to="contact">
-                {" "}
-                <Link to="contact">Contact Us</Link>
-              </Button>
+              <Link to="contact" className={`${styles.navbtn} ${styles.display}`}>Contact Us</Link>
             </div>
           )}
         </div>
--- app/src/pages/home/Ready.jsx
+++ app/src/pages/home/Ready.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Button from "../../components/Button";
 import styles from "../../styles/home.module.scss";
 import { Link } from "react-router-dom";
 
@@ -10,7 +9,7 @@ function Ready() {
       <div className={styles.wrapper}>
         <div className={styles.content}>
           <h2>Ready to get started?</h2>
-          <Button><Link to="contact">contact us</Link></Button>
+          <Link to="contact">contact us</Link>
         </div>
       </div>
     </div>
