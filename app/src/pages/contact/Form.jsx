f42baa4cd533f1b49bbcdbc3c9b302d82b18cfc9
Jio Bautista <98562544+JioBautista@users.noreply.github.com>
2023-11-18 14:14:45 -0500

    Project completed

--- app/src/pages/contact/Form.jsx
+++ app/src/pages/contact/Form.jsx
@@ -1,16 +1,59 @@
 import React from "react";
 import styles from "../../styles/contact.module.scss";
+import { useForm } from "react-hook-form";
 
 function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(JSON.stringify(data));
   return (
     <div className={styles.form}>
-      <form>
-        <input placeholder="Name"></input>
-        <input placeholder="Email Address"></input>
-        <input placeholder="Company Name"></input>
-        <input placeholder="Title"></input>
-        <textarea placeholder="Message"></textarea>
-        <button>submit</button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          placeholder="Name"
+          {...register("name", { required: true })}
+          aria-invalid={errors.name ? "true" : "false"}
+        ></input>
+
+        {errors.name?.type === "required" && (
+          <span role="alert">This field is required</span>
+        )}
+
+        <input
+          placeholder="Email Address"
+          {...register("email", { required: true })}
+        ></input>
+
+        {errors.email?.type === "required" && (
+          <span role="alert">This field is required</span>
+        )}
+
+        <input
+          placeholder="Company Name"
+          {...register("companyName", { required: true })}
+        ></input>
+
+        {errors.companyName?.type === "required" && (
+          <span role="alert">This field is required</span>
+        )}
+        <input
+          placeholder="Title"
+          {...register("title", { required: true })}
+        ></input>
+
+        {errors.title?.type === "required" && (
+          <span role="alert">This field is required</span>
+        )}
+        <textarea
+          placeholder="Message"
+          {...register("message")}
+        ></textarea>
+
+        <button type="submit">submit</button>
       </form>
     </div>
   );
